05ee71f9dc2b985857330d184713cf3ff1916baa
cs44445 <2475546459@qq.com>
2022-01-06 14:30:14 +0800

    完成购物车详情

--- src/components/ShopCar/index.vue
+++ src/components/ShopCar/index.vue
@@ -1,34 +1,72 @@
 <template>
   <div class="shop-car">
-    <div class="left">
-      <div
-        class="logo-wrapper"
-        :style="{ background: totalPrice > 0 ? '#ffd161' : '' }"
-      >
+    <div class="shop-car-wrapper">
+      <div class="left">
         <div
-          class="icon-shopping_cart"
-          :style="{ color: totalPrice > 0 ? '#2d2b2a' : '' }"
-        ></div>
-        <div class="logo-num" v-show="totalPrice > 0">{{ totalCount }}</div>
-      </div>
-
-      <div class="price-wrapper">
-        <div class="price" v-show="totalPrice > 0">￥{{ totalPrice }}</div>
-        <div
-          class="default"
-          :style="{ lineHeight: totalPrice > 0 ? '12px' : '51px' }"
+          class="logo-wrapper"
+          :style="{ background: totalPrice > 0 ? '#ffd161' : '' }"
+          @click="clickCar"
         >
-          另需{{ priceInfo.shipping_fee_tip }}
+          <div
+            class="icon-shopping_cart"
+            :style="{ color: totalPrice > 0 ? '#2d2b2a' : '' }"
+          ></div>
+          <div class="logo-num" v-show="totalPrice > 0">{{ totalCount }}</div>
+        </div>
+
+        <div class="price-wrapper">
+          <div class="price" v-show="totalPrice > 0">￥{{ totalPrice }}</div>
+          <div
+            class="default"
+            :style="{ lineHeight: totalPrice > 0 ? '12px' : '51px' }"
+          >
+            另需{{ priceInfo.shipping_fee_tip }}
+          </div>
         </div>
       </div>
+      <div class="default-pay" :class="totalPrice > 0 ? 'to-pay' : ''">
+        {{ showTip }}
+      </div>
     </div>
-    <div class="default-pay" :class="totalPrice > 0 ? 'to-pay' : ''">
-      {{ showTip }}
+    <!-- 购物车详情 -->
+    <div class="shop-car-list" v-show="isShowCar || showList">
+      <div class="list-head" v-if="priceInfo.discounts2">
+        {{ priceInfo.discounts2[0].info }}
+      </div>
+      <div class="list-clean-warpper">
+        <div>1号口袋</div>
+        <div class="clean-box" @click="cleanShopCar">
+          <img src="../../assets/img/delete.png" alt="" class="delete-icon" />
+          <div>清空购物车</div>
+        </div>
+      </div>
+      <div class="list-main">
+        <div class="item-box" v-for="(food, index) in selectFood" :key="index">
+          <div class="item-info">
+            <div class="left-des">
+              <div class="left-name">{{ food.name }}</div>
+              <div class="des">
+                {{ food.description ? food.description : food.unit }}
+              </div>
+            </div>
+            <div class="right-price">￥{{ food.min_price }}</div>
+          </div>
+          <div class="car-wrapper">
+            <CarControl :food="food" />
+          </div>
+        </div>
+      </div>
+      <div class="list-footer"></div>
     </div>
+
+    <!-- 底部遮罩 -->
+    <div class="car-mask" v-show="isShowCar"></div>
   </div>
 </template>
 
 <script>
+import CarControl from "../common/CarControl";
+
 export default {
   name: "ShopCar",
   props: {
@@ -48,11 +86,27 @@ export default {
   data() {
     return {
       // totalPrice: 0,
+      isShowCar: false,
     };
+  },
+  components: {
+    CarControl,
   },
   methods: {
     getTotal() {
       console.log("hhh");
+    },
+    clickCar() {
+      if (!this.totalCount) {
+        return;
+      }
+      this.isShowCar = !this.isShowCar;
+    },
+    cleanShopCar() {
+      this.selectFood.forEach((food) => {
+        food.count = 0;
+      });
+      this.isShowCar = false;
     },
   },
   computed: {
@@ -77,6 +131,11 @@ export default {
       });
       return total;
     },
+    showList() {
+      if (this.totalCount === 0) {
+        this.isShowCar = false;
+      }
+    },
   },
 };
 </script>
@@ -84,7 +143,7 @@ export default {
 <style lang="less" scoped>
 @import url("../../assets/style/icon.css");
 
-.shop-car {
+.shop-car-wrapper {
   width: 100%;
   height: 51px;
   position: fixed;
@@ -93,6 +152,7 @@ export default {
   background-color: #514f4f;
   display: flex;
   color: white;
+  z-index: 99;
 
   .left {
     display: flex;
@@ -160,5 +220,101 @@ export default {
   .light {
     background-color: #ffd161;
   }
+}
+
+.shop-car-list {
+  position: fixed;
+  left: 0;
+  bottom: 51px;
+  width: 100%;
+  background-color: #fff;
+  z-index: 98;
+
+  .list-head {
+    height: 30px;
+    background-color: #f3e6c6;
+    color: #646158;
+    font-size: 11px;
+    text-align: center;
+    line-height: 30px;
+  }
+
+  .list-clean-warpper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10px 0 6px;
+    border-left: 4px solid #53c123;
+    background-color: #f4f4f4;
+    height: 30px;
+
+    .clean-box {
+      display: flex;
+      .delete-icon {
+        width: 14px;
+        height: 14px;
+        margin-right: 4px;
+      }
+    }
+  }
+
+  .list-main {
+    max-height: 360px;
+    overflow: scroll;
+
+    .item-box {
+      height: 38px;
+      padding: 12px 12px 10px 12px;
+      font-size: 16px;
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid #eee;
+
+      .item-info {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        // border-bottom: 1px solid #eee;
+        padding-bottom: 10px;
+        width: 240px;
+
+        .left-des {
+          .left-name {
+            -webkit-line-clamp: 1;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+            width: 168px;
+          }
+          .des {
+            color: #b4b4b4;
+            -webkit-line-clamp: 1;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+            width: 168px;
+            font-size: 12px;
+          }
+        }
+      }
+    }
+  }
+
+  .list-footer {
+    height: 11px;
+    width: 100%;
+    background-color: #f4f4f4;
+    border-left: 4px solid #ffd161;
+  }
+}
+
+.car-mask {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 3;
+  background: rgba(7, 17, 27, 0.6);
 }
 </style>
